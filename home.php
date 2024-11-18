c4b8b9757e1b9ebfd2c2f8c825c5a105ccb1e21e
diprama <50134297+diprama@users.noreply.github.com>
2024-11-18 16:50:47 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,6 +6,11 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js">
 
+<?php 
+		include "index.php"; 
+
+?>
+
 <!--<![endif]-->
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
